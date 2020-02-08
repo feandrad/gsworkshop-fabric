12ff03866940f2868ae3d5f255e989a587aabf72
Felipe Andrade <feandrad@gmail.com>
2020-02-08 17:41:46 -0300

    copper, tin, bronze and silver done

--- src/main/java/io/felipeandrade/gsw2/block/ColorProviderMaterialBlock.java
+++ src/main/java/io/felipeandrade/gsw2/block/ColorProviderMaterialBlock.java
@@ -3,19 +3,21 @@ package io.felipeandrade.gsw2.block;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.client.color.item.ItemColorProvider;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 
 import io.felipeandrade.gsw2.common.ClientProxyInit;
 import io.felipeandrade.gsw2.material.GSWMaterial;
 
-public class ColorProviderMaterialBlock extends GSWMaterialBlock implements BlockColorProvider, ClientProxyInit {
+public class ColorProviderMaterialBlock extends GSWMaterialBlock
+        implements BlockColorProvider, ItemColorProvider, ClientProxyInit {
 
 
     public ColorProviderMaterialBlock(GSWMaterial material, Settings settings) {
         super(material, settings);
     }
-
 
     @Override
     public int getColor(BlockState state, BlockRenderView view, BlockPos pos, int tintIndex) {
@@ -23,8 +25,14 @@ public class ColorProviderMaterialBlock extends GSWMaterialBlock implements Bloc
     }
 
     @Override
+    public int getColor(ItemStack stack, int tintIndex) {
+        return material.getSecondaryColor();
+    }
+
+    @Override
     public void onClientInit() {
         ColorProviderRegistry.BLOCK.register(this, this);
+        ColorProviderRegistry.ITEM.register(this, this);
     }
 
 }
--- src/main/java/io/felipeandrade/gsw2/block/ColorProviderOreBlock.java
+++ src/main/java/io/felipeandrade/gsw2/block/ColorProviderOreBlock.java
@@ -3,13 +3,15 @@ package io.felipeandrade.gsw2.block;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.client.color.item.ItemColorProvider;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 
 import io.felipeandrade.gsw2.common.ClientProxyInit;
 import io.felipeandrade.gsw2.material.GSWMaterial;
 
-public class ColorProviderOreBlock extends GSWOreBlock implements BlockColorProvider {
+public class ColorProviderOreBlock extends GSWOreBlock implements BlockColorProvider, ItemColorProvider {
 
     public ColorProviderOreBlock(GSWMaterial material, Settings settings) {
         super(material, settings);
@@ -21,8 +23,15 @@ public class ColorProviderOreBlock extends GSWOreBlock implements BlockColorProv
     }
 
     @Override
+    public int getColor(ItemStack stack, int tintIndex) {
+        return material.getSecondaryColor();
+    }
+
+    @Override
     public void onClientInit() {
         super.onClientInit();
         ColorProviderRegistry.BLOCK.register(this, this);
+        ColorProviderRegistry.ITEM.register(this, this);
     }
+
 }
