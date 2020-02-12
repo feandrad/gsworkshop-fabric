56b36dc5319f48d8ac1a75be74029057ea6e27f0
Felipe Andrade <feandrad@gmail.com>
2020-02-12 08:05:25 -0300

    gem ore generation

--- src/main/java/io/felipeandrade/gsw2/world/GSWWorldGen.java
+++ src/main/java/io/felipeandrade/gsw2/world/GSWWorldGen.java
@@ -14,6 +14,10 @@ import net.minecraft.world.gen.feature.OreFeatureConfig.Target;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.felipeandrade.gsw2.material.gem.AmethystMaterial;
+import io.felipeandrade.gsw2.material.gem.RubyMaterial;
+import io.felipeandrade.gsw2.material.gem.SapphireMaterial;
+import io.felipeandrade.gsw2.material.gem.TopazMaterial;
 import io.felipeandrade.gsw2.material.metal.CopperMaterial;
 import io.felipeandrade.gsw2.material.metal.PlatinumMaterial;
 import io.felipeandrade.gsw2.material.metal.SilverMaterial;
@@ -25,9 +29,14 @@ public class GSWWorldGen {
 
     private static final RangeDecoratorConfig COPPER_RANGE = new RangeDecoratorConfig(16, 30, 30, 80);
     private static final RangeDecoratorConfig TIN_RANGE = new RangeDecoratorConfig(16, 30, 30, 80);
-    private static final RangeDecoratorConfig SILVER_RANGE = new RangeDecoratorConfig(16, 10, 10, 40);
-    private static final RangeDecoratorConfig TITANIUM_RANGE = new RangeDecoratorConfig(1, 3, 3, 15);
-    private static final RangeDecoratorConfig PLATINUM_RANGE = new RangeDecoratorConfig(1, 3, 3, 15);
+    private static final RangeDecoratorConfig SILVER_RANGE = new RangeDecoratorConfig(16, 10, 10, 60);
+    private static final RangeDecoratorConfig TITANIUM_RANGE = new RangeDecoratorConfig(1, 3, 3, 20);
+    private static final RangeDecoratorConfig PLATINUM_RANGE = new RangeDecoratorConfig(1, 3, 3, 20);
+
+    private static final RangeDecoratorConfig TOPAZ_RANGE = new RangeDecoratorConfig(1, 4, 4, 40);
+    private static final RangeDecoratorConfig AMETHYST_RANGE = new RangeDecoratorConfig(1, 4, 4, 40);
+    private static final RangeDecoratorConfig RUBY_RANGE = new RangeDecoratorConfig(1, 4, 4, 40);
+    private static final RangeDecoratorConfig SAPPHIRE_RANGE = new RangeDecoratorConfig(1, 4, 4, 40);
 
     private static List<Biome> checkedBiomes = new ArrayList<>();
 
@@ -53,6 +62,22 @@ public class GSWWorldGen {
             addOre(biome, SilverMaterial.ORE_BLOCK, 6, SILVER_RANGE);
             addOre(biome, TitaniumMaterial.ORE_BLOCK, 3, TITANIUM_RANGE);
             addOre(biome, PlatinumMaterial.ORE_BLOCK, 3, PLATINUM_RANGE);
+
+            if(Biome.Category.MESA == biome.getCategory()) {
+                addOre(biome, RubyMaterial.ORE_BLOCK, 4, RUBY_RANGE);
+            }
+
+            if(Biome.Category.JUNGLE == biome.getCategory()){
+                addOre(biome, AmethystMaterial.ORE_BLOCK, 4, AMETHYST_RANGE);
+            }
+
+            if(Biome.Category.DESERT == biome.getCategory()){
+                addOre(biome, TopazMaterial.ORE_BLOCK, 4, TOPAZ_RANGE);
+            }
+
+            if(Biome.Category.ICY == biome.getCategory()){
+                addOre(biome, SapphireMaterial.ORE_BLOCK, 4, SAPPHIRE_RANGE);
+            }
         }
 
     }
