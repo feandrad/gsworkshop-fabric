cde8d2c327c2ca9f79be46ed6161fd4e60b1c7aa
Felipe Andrade <feandrad@gmail.com>
2022-11-06 11:09:56 -0300

    removed duplicated copper recipes

--- gsw-mod/src/main/kotlin/io/felipeandrade/gsw/material/metal/CopperMaterial.kt
+++ gsw-mod/src/main/kotlin/io/felipeandrade/gsw/material/metal/CopperMaterial.kt
@@ -1,10 +1,6 @@
 package io.felipeandrade.gsw.material.metal
 
 import io.felipeandrade.gsw.GSWItemGroup
-import io.felipeandrade.gsw.block.ColorProviderMaterialBlock
-import io.felipeandrade.gsw.block.ColorProviderOreBlock
-import io.felipeandrade.gsw.block.GSWBlock
-import io.felipeandrade.gsw.block.GSWMaterialBlock
 import io.felipeandrade.gsw.common.GSWColor
 import io.felipeandrade.gsw.item.GSWItem
 import io.felipeandrade.gsw.item.tool.*
@@ -12,15 +8,14 @@ import io.felipeandrade.gsw.material.ColorProviderMaterialItem
 import io.felipeandrade.gsw.material.GSWMaterial
 import io.felipeandrade.gsw.material.GSWMaterialItem
 import net.minecraft.item.Item
+import net.minecraft.item.Items
 
 class CopperMaterial : GSWMaterial("copper", GSWColor.dark_orange) {
-    override fun allItems(): List<GSWItem> = listOf(INGOT, NUGGET, DUST, PLATE, GEAR, WIRE)
-    override fun allBlocks(): List<GSWBlock> = listOf(ORE_BLOCK, METAL_BLOCK)
+    override fun allItems(): List<GSWItem> = listOf(NUGGET, DUST, PLATE, GEAR, WIRE)
     override fun allTools(): List<GSWTool> = listOf(SWORD, PICKAXE, AXE, SHOVEL, HOE, HAMMER)
 
     companion object {
         val MATERIAL: GSWMaterial = CopperMaterial()
-        val INGOT: GSWItem = ColorProviderMaterialItem("ingot", MATERIAL, Item.Settings().group(GSWItemGroup.MATERIALS))
         val NUGGET: GSWMaterialItem =
             ColorProviderMaterialItem("nugget", MATERIAL, Item.Settings().group(GSWItemGroup.MATERIALS))
         val DUST: GSWMaterialItem =
@@ -31,9 +26,7 @@ class CopperMaterial : GSWMaterial("copper", GSWColor.dark_orange) {
             ColorProviderMaterialItem("gear", MATERIAL, Item.Settings().group(GSWItemGroup.MATERIALS))
         val WIRE: GSWMaterialItem =
             ColorProviderMaterialItem("wire", MATERIAL, Item.Settings().group(GSWItemGroup.MATERIALS))
-        val METAL_BLOCK: GSWBlock = ColorProviderMaterialBlock(MATERIAL, GSWMaterialBlock.SETTINGS_METAL)
-        val ORE_BLOCK: GSWBlock = ColorProviderOreBlock(MATERIAL, GSWMaterialBlock.SETTINGS_ORE)
-        val TOOL_MATERIAL = GSWToolMaterial(1, 160, 5.0f, 1.6f, 16, INGOT)
+        val TOOL_MATERIAL = GSWToolMaterial(1, 160, 5.0f, 1.6f, 16, Items.COPPER_INGOT)
         val SWORD: GSWTool = GSWSword(MATERIAL, TOOL_MATERIAL, Item.Settings().group(GSWItemGroup.TOOLS))
         val PICKAXE: GSWTool = GSWPickaxe(MATERIAL, TOOL_MATERIAL, Item.Settings().group(GSWItemGroup.TOOLS))
         val AXE: GSWTool = GSWAxe(MATERIAL, TOOL_MATERIAL, Item.Settings().group(GSWItemGroup.TOOLS))
